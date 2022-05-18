40b8b3258f5d7f6fb0d00fc99b8362a921a693c6
Oscar Izquierdo Cabezas <oscarizqui11@gmail.com>
2022-05-18 21:10:36 +0200

    Camera movement between levels
    Además he descubierto las curvas de animaciones suuuuu

--- Assets/Script/ChangeLevelBehaviour.cs
+++ Assets/Script/ChangeLevelBehaviour.cs
@@ -6,21 +6,37 @@ public class ChangeLevelBehaviour : MonoBehaviour
 {
     private Camera _mainCamera;
 
+    private float vertilcalCamSize;
+    private float horizontalCamSize;
+
     // Start is called before the first frame update
     void Start()
     {
         _mainCamera = Camera.main;
+
+        vertilcalCamSize = Camera.main.orthographicSize * 2.0f;
+        horizontalCamSize = vertilcalCamSize * Screen.width / Screen.height;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(transform.position.y - _mainCamera.transform.position.y < -vertilcalCamSize / 2)
+        {
+            _mainCamera.transform.position = new Vector3(_mainCamera.transform.position.x, _mainCamera.transform.position.y - vertilcalCamSize, _mainCamera.transform.position.z);
+        }
+        else if (transform.position.y - _mainCamera.transform.position.y > vertilcalCamSize / 2)
+        {
+            _mainCamera.transform.position = new Vector3(_mainCamera.transform.position.x, _mainCamera.transform.position.y + vertilcalCamSize, _mainCamera.transform.position.z);
+        }
+        else if (transform.position.x - _mainCamera.transform.position.x < -horizontalCamSize / 2)
+        {
+            _mainCamera.transform.position = new Vector3(_mainCamera.transform.position.x - horizontalCamSize, _mainCamera.transform.position.y, _mainCamera.transform.position.z);
+        }
+        else if (transform.position.x - _mainCamera.transform.position.x > horizontalCamSize / 2)
+        {
+            _mainCamera.transform.position = new Vector3(_mainCamera.transform.position.x + horizontalCamSize, _mainCamera.transform.position.y, _mainCamera.transform.position.z);
+        }
 
-    private void OnBecameInvisible()
-    {
-        _mainCamera.transform.position = new Vector3(0, _mainCamera.transform.position.y - _mainCamera.transform.localScale.y, _mainCamera.transform.position.z);
     }
-
 }
